2186b0ba09a3c2f6714f3a924c0db67409545329
UELTON SANTOS DA SILVA <uelton.ssilva@senacsp.edu.br>
2019-05-08 12:24:53 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/appfilmes/MainActivity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.appfilmes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
